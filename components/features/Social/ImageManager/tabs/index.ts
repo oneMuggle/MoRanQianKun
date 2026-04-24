58fb84dff76b09e3b11a10df6d502a557bd3d553
onemuggle <2397801926@qq.com>
2026-04-24 11:21:13 +0800

    refactor(ImageManager): 将预设标签页提取为独立组件

--- components/features/Social/ImageManager/tabs/index.ts
+++ components/features/Social/ImageManager/tabs/index.ts
@@ -4,3 +4,4 @@ export { LibraryTab } from './LibraryTab';
 export { RulesTab } from './RulesTab';
 export { SceneTab } from './SceneTab';
 export { ManualTab } from './ManualTab';
+export { PresetsTab } from './PresetsTab';
